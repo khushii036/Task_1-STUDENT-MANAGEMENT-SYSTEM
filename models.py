63af93926c5e5982c50d750fe5ed467eb97cb118
khushi kumari <khushivns8865@gmail.com>
2026-06-05 02:40:55 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, student_id, name, age, course):
-        self.student_id = student_id
-        self.name = name
-        self.age = age
-        self.course = course
-
-    def __str__(self):
-        return f"ID: {self.student_id} | Name: {self.name} | Age: {self.age} | Course: {self.course}"
